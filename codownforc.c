aa314b94c63b95d674b0c75829b65ec1df7ba2db
Rohan Kasabe <71020327+ROHAN0011@users.noreply.github.com>
2021-10-28 19:28:15 +0530

    Update codownforc.c

--- codownforc.c
+++ codownforc.c
@@ -5194,3 +5194,269 @@ int main() {
     return 0;
 }      
             
+
+            
+/*  141. WAP to multiply and add two complex numbers
+
+Input :
+5
+-4
+4
+6
+
+Output :
+Sum is 9 + 2i
+Product is 44 + 14i                 */
+
+#include<stdio.h>
+#include<conio.h>
+
+typedef struct {
+  int real;
+  int imag;
+} complex;
+
+void getdata (complex *);
+void display (complex);
+complex sum(complex,complex);
+complex mult(complex,complex);
+
+main() {
+ complex c1,c2,c3,c4;
+ getdata(&c1);
+ getdata(&c2);
+ 
+ c3=sum(c1,c2);
+ printf("Sum is ");
+ display(c3);
+ 
+ c4=mult(c1,c2);
+ printf("Product is ");
+ display(c4);
+ 
+ getch();
+}
+
+void getdata(complex *p) {
+ printf("Enter real  ");
+ scanf("%d",&p->real);
+ printf("Enter imag ");
+ scanf("%d",&p->imag);
+}
+
+complex sum(complex c1,complex c2) {
+ complex t;
+ t.real=c1.real+c2.real;
+ t.imag=c1.imag+c2.imag;
+ return t;
+}
+
+complex mult(complex c1,complex c2) {
+ complex t;
+ t.real=c1.real*c2.real-c1.imag*c2.imag;
+ t.imag=c1.real*c2.imag+c2.real*c1.imag;
+ return t;
+}
+
+void display {
+ if(c.imag>=0)
+ printf("%d+%di\n",c.real,c.imag);
+ else
+ printf("%d%di\n",c.real,c.imag);
+}
+
+            
+            
+// 142. Program to concatenate two strings using pointer.
+
+#include <stdio.h>
+
+void concatenator(char *str, char *substr) {
+
+    while (*str)
+        str++;
+
+    while (*substr) {
+        *str = *substr;
+        substr++;
+        str++;
+    }
+
+    *str = '\0';
+}
+
+int main() {
+
+    char str[100], substr[100];
+
+    printf("Enter the source string : ");
+    gets(str);
+
+    printf("\nEnter string to concatenate : ");
+    gets(substr);
+
+    concatenator(str, substr);
+
+    printf("String after concatenation is \"%s\"\n", str);
+
+    return 0;
+}
+
+            
+            
+// 143. Program to allocate memory using malloc() and free memory using free().
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+    int n, i, *ptr, sum = 0;
+    printf("Enter total number of elements : ");
+    scanf("%d", &n);
+
+    ptr = (int *) malloc(n * sizeof(int));
+
+    if (ptr == NULL) {
+        printf("Error! Memory not allocated.");
+        return 0;
+    }
+
+    printf("Enter elements of array : \n");
+    for (i = 0; i < n; ++i) {
+        scanf("%d", ptr + i);
+        sum += *(ptr + i);
+    }
+
+    printf("Elements are :\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\n", ptr[i]);
+    }
+
+    free(ptr);
+    return 0;
+}
+
+            
+            
+// 144. Circular Link List.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+} node;
+
+void insert(node *pointer, int data)
+{
+    node *start = pointer;
+
+    while (pointer->next != start)
+    {
+        pointer = pointer->next;
+    }
+    
+    pointer->next = (node *)
+    malloc(sizeof(node));
+    pointer = pointer->next;
+    pointer->data = data;
+    pointer->next = start;
+    printf("%d entered.\n", data);
+}
+
+int find(node *pointer, int key)
+{
+    node *start = pointer;
+    pointer = pointer->next;
+    while (pointer != start)
+    {
+        if (pointer->data == key)
+        {
+            return 1;
+        }
+        pointer = pointer->next;
+    }
+    return 0;
+}
+
+void delete(node *pointer, int data)
+{
+    node *start = pointer;
+    while(pointer->next!=start &&
+    (pointer->next)->data != data)
+    {
+        pointer = pointer->next;
+    }
+    if(pointer->next==start)
+    {
+        printf("Element %d is not present in the list\n",data);
+        return;
+    }
+    node *temp;
+    temp = pointer->next;
+    printf("%d deleted.\n",data);
+    pointer->next = temp->next;
+    free(temp);
+    return;
+}
+
+void print(node *start, node *pointer)
+{
+    if (pointer == start)
+    {
+        return;
+    }
+    printf("%d ", pointer->data);
+    print(start, pointer->next);
+}
+
+int main()
+{
+    node *start, *temp;
+    start = (node *) malloc(sizeof(node));
+    temp = start;
+    temp->next = start;
+    printf("1. Insert\n");
+    printf("2. Delete\n");
+    printf("3. Print\n");
+    printf("4. Find\n");
+    while (1)
+    {
+        int query;
+        scanf("%d", &query);
+        if (query == 1)
+        {
+            int data;
+            scanf("%d", &data);
+            insert(start, data);
+        }
+        else if (query == 2)
+        {
+            int data;
+            scanf("%d", &data);
+            delete (start, data);
+        }
+        else if (query == 3)
+        {
+            printf("The list is ");
+            print(start, start->next);
+            printf("\n");
+        }
+        else if (query == 4)
+        {
+            int data;
+            scanf("%d", &data);
+            int status = find(start, data);
+            if (status)
+            {
+                printf("Element Found\n");
+            }
+            else
+            {
+                printf("Element Not Found\n");
+            }
+        }
+    }
+}            
